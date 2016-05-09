9a11c880a58ba05e71f80c7ce8c4f89bc9151dc6
MathieuIpii <math_6659@hotmail.com>
2016-05-09 16:52:41 +0200

    position courante
    ajout du code pour gérer la position actuelle

--- JaiFaim/app/src/main/java/com/margotelmadi/jaifaim/ListRestoActivity.java
+++ JaiFaim/app/src/main/java/com/margotelmadi/jaifaim/ListRestoActivity.java
@@ -2,14 +2,19 @@ package com.margotelmadi.jaifaim;
 
 import android.app.AlertDialog;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
@@ -18,18 +23,38 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.Api;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.margotelmadi.jaifaim.common.listener.OnRecyclerItemClickListener;
 import com.margotelmadi.jaifaim.factory.ListResto;
 import com.margotelmadi.jaifaim.factory.RestoFactory;
 import com.margotelmadi.jaifaim.model.LieuResto;
 import com.margotelmadi.jaifaim.model.Restaurant;
 
+import java.io.FileDescriptor;
+import java.io.PrintWriter;
+import java.text.DateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.jar.Manifest;
 
-public class ListRestoActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, OnRecyclerItemClickListener {
+public class ListRestoActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, OnRecyclerItemClickListener,
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener{
 
+    private static final int MY_PERMISSIONS_REQUEST_LOCATION = 1;
     private List<LieuResto> mLieuResto;
+    private GoogleApiClient mGoogleApiClient;
+    private LocationRequest mLocationRequest;
+    private Location mCurrentLocation;
+    private String mLastUpdateTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +68,12 @@ public class ListRestoActivity extends AppCompatActivity implements NavigationVi
         //Retourne type resto cliqué
         getTypeResto(typeResto);
 
+        //gestion position courante
+        setSupportActionBar(toolbar);
+        assert getActionBar() != null;
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        buildGoogleApiClient();
 
         //Affichage liste
         mLieuResto = ListResto.getListRestoList();
@@ -66,7 +97,88 @@ public class ListRestoActivity extends AppCompatActivity implements NavigationVi
         });
     }
 
+    private synchronized void buildGoogleApiClient() {
+        if(mGoogleApiClient == null){
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+            createLocationRequest();
+        }
+    }
+
+
+    private void createLocationRequest() {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(10000);
+        mLocationRequest.setFastestInterval(5000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
     @Override
+    protected void onStart(){
+        mGoogleApiClient.connect();
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop(){
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        if(mGoogleApiClient.isConnected()){
+            startLocationUpdates();
+        }
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        if(mGoogleApiClient.isConnected()){
+            stopLocationUpdates();
+        }
+    }
+
+    private void stopLocationUpdates() {
+        LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+    }
+
+    private void startLocationUpdates() {
+        if(ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[]{
+                    android.Manifest.permission.ACCESS_FINE_LOCATION
+            }, MY_PERMISSIONS_REQUEST_LOCATION);
+        }else{
+            if(mCurrentLocation == null){
+                mCurrentLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+                mLastUpdateTime = DateFormat.getDateTimeInstance().format(new Date());
+                updateUI();
+            }
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+        }
+
+    }
+
+    //@Override
+    public void OnRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults){
+        if(requestCode == MY_PERMISSIONS_REQUEST_LOCATION) {
+            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                startLocationUpdates();
+            }
+        }
+    }
+
+    private void updateUI() {
+        if(mCurrentLocation != null){
+            // mis à jour de l'interface
+        }
+    }
+
     public boolean onNavigationItemSelected(MenuItem item) {
         return false;
     }
@@ -87,4 +199,25 @@ public class ListRestoActivity extends AppCompatActivity implements NavigationVi
         return typeResto;
     }
 
+    @Override
+    public void onConnected(Bundle pBundle) {
+        startLocationUpdates();
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult pConnectionResult) {
+        Snackbar.make(findViewById(R.id.toolbar), R.string.connection_location_failed, Snackbar.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onLocationChanged(Location pLocation) {
+        mCurrentLocation = pLocation;
+        mLastUpdateTime = DateFormat.getDateTimeInstance().format(new Date());
+        updateUI();
+    }
 }

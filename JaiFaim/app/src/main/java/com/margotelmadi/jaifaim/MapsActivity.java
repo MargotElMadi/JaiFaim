8f911607b6c1242b17feeb12b19822dbec40d336
MargotElMadi <margotelmadi@MacBook-Pro-de-Margot.local>
2016-05-12 12:07:00 +0200

    maps

--- JaiFaim/app/src/main/java/com/margotelmadi/jaifaim/MapsActivity.java
+++ JaiFaim/app/src/main/java/com/margotelmadi/jaifaim/MapsActivity.java
@@ -1,30 +1,15 @@
 package com.margotelmadi.jaifaim;
 
-import android.app.Activity;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.location.Location;
-import android.location.LocationListener;
-import android.net.Uri;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.widget.Toolbar;
+import android.support.v4.app.FragmentActivity;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.margotelmadi.jaifaim.factory.RestoFactory;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -55,6 +40,9 @@ public class MapsActivity extends Activity{
 }
 */
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

b96f8865091c8a0bfe581545d3e3f83858776d3f
MathieuIpii <math_6659@hotmail.com>
2016-05-13 19:55:55 +0200

    resto
    ajout des restos

--- JaiFaim/app/src/main/java/com/margotelmadi/jaifaim/factory/ListResto.java
+++ JaiFaim/app/src/main/java/com/margotelmadi/jaifaim/factory/ListResto.java
@@ -20,12 +20,15 @@ public class ListResto {
 
     public static List<LieuResto> getListRestoList(){
         List<LieuResto> listResto = new ArrayList<>();
-        listResto.add(new LieuResto("Burger", "Speed Burger", 50.6288, 3.0530));
-        listResto.add(new LieuResto("Burger", "Buffalo Burger", 50.6369, 3.0657));
-        listResto.add(new LieuResto("Pizza", "", , ));
-        listResto.add(new LieuResto("Pizza", "", , ));
-        listResto.add(new LieuResto("Kebab", "", , ));
-        listResto.add(new LieuResto("Kebab", "", , ));
+        listResto.add(new LieuResto("Burger", "Speed Burger", 50.6287687, 3.0530392));
+        listResto.add(new LieuResto("Burger", "Buffalo Burger", 50.636969, 3.0657086));
+        listResto.add(new LieuResto("Burger", "Taxi Burger", 50.6307738, 3.0556791));
+        listResto.add(new LieuResto("Pizza", "L'Emilepates", 50.628327, 3.0605309));
+        listResto.add(new LieuResto("Pizza", "Pizza-jazz", 50.6305144, 3.0508304));
+        listResto.add(new LieuResto("Pizza", "Masséna Pizza", 50.633464, 3.0549603));
+        listResto.add(new LieuResto("Kebab", "Kebab & Co", 50.6315199, 3.0540235));
+        listResto.add(new LieuResto("Kebab", "Snack Tigo", 50.6316441, 3.0494705));
+        listResto.add(new LieuResto("Kebab", "Kebab GUR", 50.6326087, 3.0625664));
         return listResto;
     }
 }
